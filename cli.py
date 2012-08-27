ddb52cc769ea3de04fcce2ae58965a454f62d655
Thomas Levine <occurrence@thomaslevine.com>
2012-08-26 22:36:07 -0400

    aoeu

--- cli.py
+++ cli.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python2
 import sys
 
-if len(sys.argv) == 0:
-    print('usage: foo <logsqlite database file>')
-else:
-    # Set database name to the first command-line argument
-    DBNAME=sys.argv[1]
+def get_db_name():
+    if len(sys.argv) == 1:
+        print('usage: %s <logsqlite database file>' % sys.argv[0])
+        exit(1)
+    else:
+        # Set database name to the first command-line argument
+        return sys.argv[1]
--- util.py
+++ util.py
@@ -1,18 +1,45 @@
 #!/usr/bin/env python2
 from dumptruck import DumpTruck
 
-def availability_within(db, uid, start=0, end=2**64):
+def get_uid_from_uid_or_nick(db, uid_or_nick):
+    'Given a uid or a nick, return a uid or an error.'
+    dt = DumpTruck(db)
+
+    # Is it a uid?
+    sql = 'SELECT count(*) FROM log_status WHERE uid = ?'
+    count = dt.execute(sql, [uid_or_nick])[0]['count(*)']
+    if count > 0:
+        # It is a uid
+        uid = uid_or_nick
+    else:
+        # It's a nick. Make sure it refers to a unique uid.
+        sql = 'SELECT count(DISTINCT uid) FROM log_status WHERE nick = ?'
+        count = dt.execute(sql, [uid_or_nick])[0]['count(DISTINCT uid)']
+
+        if count == 1:
+            # It is distinct
+            sql = 'SELECT uid FROM log_status WHERE nick = ? LIMIT 1'
+            uid = dt.execute(sql, [uid_or_nick])[0]['uid']
+
+        else:
+            # Multiple people have this name.
+            raise ValueError('The name you specified does not refer uniquely to one user.')
+    return uid
+
+def avail_within(db, start=0, end=2**32):
     '''
     Given a database filename, uid and a time range (POSIX times),
-    return how long the person was online.
+    return how long everyone was online, in seconds.
     '''
+
+    # Database connection
     dt = DumpTruck(db)
     updates = dt.execute('''
-SELECT uid, ts, status
-FROM log_status
-ORDER BY uid, ts
-WHERE ts > ? AND ts < ?;
-''', [start, end])
+        SELECT uid, ts, status
+        FROM log_status
+        WHERE ts > ? AND ts < ?
+        ORDER BY uid, ts
+        ;''', [start, end])
 
     # This loop depends on the order by clause above
     total_time = {}
@@ -24,21 +51,46 @@ WHERE ts > ? AND ts < ?;
 
         elif u['status'] == 'avail' and (u['uid'] not in total_time):
             # First time we see person becoming available
-            total_time[u[uid]] = 0
+            total_time[u['uid']] = 0
 
-        elif update['status'] == 'avail':
+        elif u['status'] == 'avail':
             # The person went avail. We don't need to do anything;
             # prev_time is recorded below the if statements
             continue
 
-        elif update['status'] == 'notavail':
+        elif u['status'] == 'notavail':
             # The person went notavail, so record the time when the user was available.
-            total_time[u[uid]] += u['ts'] - prev_time
+            total_time[u['uid']] += u['ts'] - prev_time
 
         else:
             raise ValueError('The update\'s status "%s" is neither "avail" nor "notavail."' % u['status'])
 
         # Record the current timestamp as prev_time
-        prev_time = update['ts']
+        prev_time = u['ts']
 
     return total_time
+
+
+if __name__ == '__main__':
+    import datetime
+    import cli
+
+    DBFILE = cli.get_db_name()
+    avail = avail_within(DBFILE)
+    dt = DumpTruck(DBFILE)
+
+    # timedeltas
+    for uid, seconds in avail.items():
+        avail[uid] = datetime.timedelta(seconds=seconds)
+
+
+    avail_by_nick={}
+
+    # Nicknames
+    for person in dt.execute('SELECT DISTINCT uid, nick FROM log_status;'):
+        pretty_key = person['nick'] + '(' + person['uid'] + ')'
+        avail_by_nick[pretty_key] = avail_by_uid[person['uid']]
+
+    # Pretty printing
+    for nick, delta in avail_by_nick.items():
+        print nick + '\t' + str(delta)
